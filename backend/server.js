fb330bc651d92cca925070907bd7c99987a9733e
HariDarshan13 <haridarshanhari123@gmail.com>
2025-09-20 10:54:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+const port = process.env.PORT||5000;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/Budget_Trip_Planner")
